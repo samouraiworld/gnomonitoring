05eb92cb56c14cf539584ceb44b8c7871fba4d90
louis <lours@samourai.coop>
2025-04-14 15:32:17 +0200

    update

--- initvalidator.sh
+++ initvalidator.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 set -e  
 
+MONIKER=${MONIKER:-"validator1"}
+PEX=${PEX:-"true"}
+PERSISTENT_PEERS=${PERSISTENT_PEERS:-"validator2:26656"}
+
 echo "if secrets exits."
 
 if [ ! -f /gnoroot/gnoland-data/secrets/node_key.json ]; then
     echo "📜 don´t have secret ."
     gnoland  secrets init 
+    gnoland config set moniker       "${MONIKER}"
+    gnoland config set p2p.pex       "${PEX}"
+    gnoland config set p2p.persistent_peers       "${PERSISTENT_PEERS}"
+
     gnoland start --lazy 
        
 fi
 
 echo "Run Gnoland"
-exec gnoland start config /gnoroot/gnoland-data/config/config.toml
+exec gnoland start  --genesis="/gnoroot/genesis.json" --log-level=debug
